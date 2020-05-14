8dcacd6c2143151c64b6d53b90a792df6eafc18d
Spencer Zou <spencerzou@ucsb.edu>
2020-05-13 22:16:00 -0700

    Updates

---
+++ precache-manifest.219918204146ac5c7db55509b9e5d744.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba4eaf09942993023580f64937b5992",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bffc5d7802ac790bdad8",
+    "url": "/static/css/main.00641505.chunk.css"
+  },
+  {
+    "revision": "18f3605f1afdc6f87d41",
+    "url": "/static/js/2.fed1cd8e.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.fed1cd8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bffc5d7802ac790bdad8",
+    "url": "/static/js/main.28dab74a.chunk.js"
+  },
+  {
+    "revision": "61df4ee0c01ad4e2b59f",
+    "url": "/static/js/runtime-main.467d6aa6.js"
+  }
+]);
