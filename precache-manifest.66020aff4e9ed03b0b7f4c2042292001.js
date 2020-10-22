0a6eac53290037af2898ac8963f495863cefc906
spenceryzou <spencerzou@ucsb.edu>
2020-10-22 10:53:19 -0700

    Updates

---
+++ precache-manifest.66020aff4e9ed03b0b7f4c2042292001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc05745237065877c4fca220feb0a000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e9f9f67635c8db2b354",
+    "url": "/static/css/main.db9f81cf.chunk.css"
+  },
+  {
+    "revision": "09d5030a036ae8d42799",
+    "url": "/static/js/2.5c516d46.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.5c516d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e9f9f67635c8db2b354",
+    "url": "/static/js/main.5b708d3a.chunk.js"
+  },
+  {
+    "revision": "61df4ee0c01ad4e2b59f",
+    "url": "/static/js/runtime-main.467d6aa6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c7c13cf29af36387df73b93f71e52309.js"
+  "/precache-manifest.66020aff4e9ed03b0b7f4c2042292001.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
